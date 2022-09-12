47b5535a11dd82d6bd771e7d087e05aa09737e23
Qudus Musiliu <quduusayo4real@gmail.com>
2022-09-12 22:48:33 +0100

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return "None"
